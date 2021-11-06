c716edff04c6f1a559b92f6d5a39666d5d0ff164
Romualdo Junior <romualdo.jrr@gmail.com>
2021-11-06 18:17:10 -0300

    adição de teste burro

---
+++ adapters/persistence-adapter/src/test/java/com/appmarket/persistence/BumpTest.java
@@ -0,0 +1,13 @@
+package com.appmarket.persistence;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BumpTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(10, 10);
+    }
+
+}
